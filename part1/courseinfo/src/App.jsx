e30224cfa9ce3400a74f359faebbebe44999cc3a
Luis Isea <19-10175@usb.ve>
2024-01-07 19:16:51 -0400

    Exercise 1.5 complete

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,23 +1,25 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  }
 
   const Header = (props) => {
     console.log('Header props:', props)
-    return <h1>{props.course}</h1>
+    return <h1>{props.course.name}</h1>
   }
 
   const Part = (props) => {
@@ -33,9 +35,9 @@ const App = () => {
     console.log('Content props:', props)
     return (
       <>
-        <Part part={props.parts[0]} />
-        <Part part={props.parts[1]} />
-        <Part part={props.parts[2]} />
+        <Part part={props.course.parts[0]} />
+        <Part part={props.course.parts[1]} />
+        <Part part={props.course.parts[2]} />
       </>
     )
   }
@@ -45,9 +47,9 @@ const App = () => {
     return (
       <p>
         Number of exercises{' '}
-        {props.parts[0].exercises +
-          props.parts[1].exercises +
-          props.parts[2].exercises}
+        {props.course.parts[0].exercises +
+          props.course.parts[1].exercises +
+          props.course.parts[2].exercises}
       </p>
     )
   }
@@ -55,8 +57,8 @@ const App = () => {
   return (
     <>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </>
   )
 }

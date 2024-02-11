9731ae95773858c564e12bbf97c67e690a3a2d2f
Luis Isea <19-10175@usb.ve>
2024-02-11 09:45:32 -0400

    Exercises 2.2 and 2.3 complete

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,6 +1,30 @@
+const Course = ({ course }) => {
+  console.log('Course props:', course)
+  return (
+    <>
+      <Header courseName={course.name} />
+      <Content courseParts={course.parts} />
+      <Total courseParts={course.parts} />
+    </>
+  )
+}
+
 const Header = ({ courseName }) => {
   console.log('Header props:', courseName)
   return <h1>{courseName}</h1>
+}
+
+const Content = ({ courseParts }) => {
+  console.log('Content props:', courseParts)
+  // Use the higher-order function map to render the parts regardless of
+  // the number of parts
+  return (
+    <>
+      {courseParts.map((part) => (
+        <Part key={part.id} coursePart={part} />
+      ))}
+    </>
+  )
 }
 
 const Part = ({ coursePart }) => {
@@ -12,25 +36,11 @@ const Part = ({ coursePart }) => {
   )
 }
 
-const Content = ({ courseParts }) => {
-  console.log('Content props:', courseParts)
-  return (
-    <>
-      {courseParts.map((part) => (
-        <Part key={part.id} coursePart={part} />
-      ))}
-    </>
-  )
-}
-
-const Course = ({ course }) => {
-  console.log('Course props:', course)
-  return (
-    <>
-      <Header courseName={course.name} />
-      <Content courseParts={course.parts} />
-    </>
-  )
+const Total = ({ courseParts }) => {
+  console.log('Total props:', courseParts)
+  // Use the higher-order function reduce to sum the exercises of all the parts
+  const total = courseParts.reduce((sum, part) => sum + part.exercises, 0)
+  return <b>total of {total} exercises</b>
 }
 
 export default Course
